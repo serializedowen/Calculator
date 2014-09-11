dc439eb13af4c540603034c392bf5c247ec8e3b5
Owen Wang <OwenWang@Owens-MacBook-Pro.local>
2014-09-11 09:43:14 -0400

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Owen Wang on 2014-09-11.
+//  Copyright (c) 2014 OwenInc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
